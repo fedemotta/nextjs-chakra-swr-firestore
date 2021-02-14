8f3ce49b2bc9a7171b819a737ef3cd8bce708fab
Federico Motta <federico.motta@softvision.com>
2021-02-14 16:58:14 -0300

    Cosmetic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,10 @@ import { Flex, Heading } from '@chakra-ui/react';
 
 export const Hero = ({ title }) => (
   <Flex justifyContent="center" alignItems="center" height="100vh">
-    <Heading fontSize="4vw">{title}</Heading>
+    <Heading fontSize="3vw">{title}</Heading>
   </Flex>
 );
 
 Hero.defaultProps = {
-  title: 'Chackra UI and SWR+Firestore Example',
+  title: '"Chackra UI" and "SWR + Firestore" example',
 };
