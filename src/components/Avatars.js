8e85a8c9cd2228fde5082793a9b164bd51e0995c
Federico Motta <federico.motta@softvision.com>
2021-02-14 20:25:21 -0300

    Listening for changes

---
+++ src/components/Avatars.js
@@ -0,0 +1,29 @@
+import { Text, Avatar, Wrap, WrapItem } from '@chakra-ui/react';
+import { useCollection } from '@nandorojo/swr-firestore';
+
+const Avatars = ({ collection }) => {
+  const { data, update, error } = useCollection(collection, {
+    listen: true,
+  });
+
+  if (error)
+    return (
+      <Wrap>
+        <Text>Error!</Text>
+      </Wrap>
+    );
+  if (!data)
+    return (
+      <Wrap>
+        <Text>Loading...</Text>
+      </Wrap>
+    );
+
+  const users = data.map((user) => (
+    <WrapItem key={user.id}>
+      <Avatar name={user.name} src={user.avatar} />
+    </WrapItem>
+  ));
+  return <Wrap>{users}</Wrap>;
+};
+export default Avatars;
