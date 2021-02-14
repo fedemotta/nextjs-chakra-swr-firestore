4388bdf8f1576c0d146fa2c14ac2cbb6e91bc411
Federico Motta <federico.motta@softvision.com>
2021-02-14 20:22:13 -0300

    Refactor as components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,4 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Avatar,
-  Wrap,
-  WrapItem,
-  List,
-  ListIcon,
-  ListItem,
-  Code,
-  UnorderedList,
-  Divider,
-} from '@chakra-ui/react';
+import { Link as ChakraLink, Text, List, ListIcon, ListItem, UnorderedList, Divider } from '@chakra-ui/react';
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
 import { Hero } from '../components/Hero';
 import { Container } from '../components/Container';
@@ -18,42 +6,8 @@ import { Main } from '../components/Main';
 import { DarkModeSwitch } from '../components/DarkModeSwitch';
 import { CTA } from '../components/CTA';
 import { Footer } from '../components/Footer';
-import { useCollection, useDocument } from '@nandorojo/swr-firestore';
-
-const userAvatars = () => {
-  const { data, update, error } = useCollection(`users`);
-
-  if (error)
-    return (
-      <Wrap>
-        <Text>Error!</Text>
-      </Wrap>
-    );
-  if (!data)
-    return (
-      <Wrap>
-        <Text>Loading...</Text>
-      </Wrap>
-    );
-
-  const users = data.map((user) => (
-    <WrapItem key={user.id}>
-      <Avatar name={user.name} src={user.avatar} />
-    </WrapItem>
-  ));
-  return <Wrap>{users}</Wrap>;
-};
-const userCodeName = (value) => {
-  const user = { id: value };
-  const { data, update, error } = useDocument(`users/${user.id}`, {
-    listen: true,
-  });
-
-  if (error) return <Code colorScheme="red">Error!</Code>;
-  if (!data) return <Code colorScheme="yellow">Loading...</Code>;
-
-  return <Code colorScheme="green">Name: {data.name}</Code>;
-};
+import Avatars from '../components/Avatars';
+import UserName from '../components/UserName';
 
 const Index = () => (
   <Container height="110vh">
@@ -63,11 +17,11 @@ const Index = () => (
       <UnorderedList>
         <ListItem>
           <Text>Get the Users collection from Firestore:</Text>
-          {userAvatars()}
+          <Avatars collection={'users'} />
         </ListItem>
         <ListItem>
           <Text>Subscribe to a specific User document from Firestore:</Text>
-          {userCodeName('c7skcokGr24yDNwVcpJy')}
+          <UserName index={'c7skcokGr24yDNwVcpJy'} />
         </ListItem>
       </UnorderedList>
 
